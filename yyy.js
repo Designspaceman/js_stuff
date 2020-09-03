c6023704ae1f49bedfc96c6ee10613b5ab04abf5
Designspaceman <steven@designspaceman.de>
2020-09-03 19:56:51 +0200

    My first commit

---
+++ yyy.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let variable_A = 25;
+let variable_B = 100;
+
+const addNumbers = (a, b, c) => {
+    return a + b;
+}
+
+let result = addNumbers(variable_A, variable_B);
+
+console.log(result);
